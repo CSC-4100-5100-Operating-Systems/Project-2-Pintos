03dd60466151fc4035228d4e92182a097e4c8bc1
LFB1937 <lewisforrestbates@hotmail.com>
2025-11-07 21:45:02 -0600

    Update thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -74,28 +74,22 @@ typedef int tid_t;
    the `magic' member of the running thread's `struct thread' is
    set to THREAD_MAGIC.  Stack overflow will normally change this
    value, triggering the assertion. */
-/* The `elem' member has a dual purpose.  It can be an element in
+/** The `elem' member has a dual purpose.  It can be an element in
    the run queue (thread.c), or it can be an element in a
    semaphore wait list (synch.c).  It can be used these two ways
    only because they are mutually exclusive: only a thread in the
    ready state is on the run queue, whereas only a thread in the
    blocked state is on a semaphore wait list. */
-struct thread
+struct thread 
   {
-    // LFB CODE LFB CODE LFB CODE LFB CODE LFB CODE LFB CODE LFB CODE LFB CODE 
-
     /* Owned by thread.c. */
     tid_t tid;                          /* Thread identifier. */
     enum thread_status status;          /* Thread state. */
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
+    int64_t wake_ticks;                 /* Absolute ticks to wake up (for alarm clock). */  // NEW FIELD
     struct list_elem allelem;           /* List element for all threads list. */
-
-    // LFB CODE LFB CODE LFB CODE LFB CODE LFB CODE LFB CODE LFB CODE LFB CODE 
-
-    /* Shared between thread.c and synch.c. */
-    struct list_elem elem;              /* List element. */
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
