79ad325158a13faa71473103fecd37089e6ad409
LFB1937 <lewisforrestbates@hotmail.com>
2025-11-07 22:15:31 -0600

    Update thread.c

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -361,7 +361,7 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  return;
+  thread_current ()->priority = new_priority;
 }
 
 /* Returns the current thread's priority. */
